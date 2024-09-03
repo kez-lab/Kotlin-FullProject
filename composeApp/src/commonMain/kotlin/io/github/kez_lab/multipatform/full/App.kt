9f2392cc786dda2790cc710f8704eee86a8ec054
KwakEuiJin <yuijin0909@gmail.com>
2024-09-03 21:35:41 +0900

    Add Ktor Client Side

--- composeApp/src/commonMain/kotlin/io/github/kez_lab/multipatform/full/App.kt
+++ composeApp/src/commonMain/kotlin/io/github/kez_lab/multipatform/full/App.kt
@@ -1,36 +1,40 @@
 package io.github.kez_lab.multipatform.full
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import kotlinfullproject.composeapp.generated.resources.Res
-import kotlinfullproject.composeapp.generated.resources.compose_multiplatform
+import io.github.kez_lab.multipatform.full.model.Task
+import io.github.kez_lab.multipatform.full.network.TaskApi
+import kotlinx.coroutines.launch
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+        val client = remember { TaskApi() }
+        val tasks = remember { mutableStateOf(emptyList<Task>()) }
+        val scope = rememberCoroutineScope()
+
+        Column(
+            Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                scope.launch {
+                    tasks.value = client.getAllTasks()
                 }
+            }) {
+                Text("Fetch Tasks")
+            }
+            for (task in tasks.value) {
+                Text(task.name)
             }
         }
     }
